08e0d0e61b345a220bfb05b4cb55df9fa078a80e
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-13 14:26:59 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+/*Mail:
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Lo svolgimento va fatto usando un ciclo for è non usando metodi degli array (come includes, per esempio) . Si puó fare??
+eh si!  Basta ragionare un po’.*/
+
+// variables and data structures
+
+// user input
+
+// list check
+
+// print result
