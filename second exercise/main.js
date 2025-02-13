82b15cf7306db31673a04e14647dcd14ec1ebf0d
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-13 14:45:09 +0100

    add variables and data structure

---
+++ second exercise/main.js
@@ -0,0 +1,12 @@
+/* Gioco dei dadi:
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda per individuare gli strumenti necessari allo sviluppo.*/
+
+// variables and data structure
+const dice = [1, 2, 3, 4, 5, 6];
+let userNumber;
+let pcNumber;
+
+// random number generators
+
+// print result
